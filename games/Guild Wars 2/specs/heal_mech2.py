1d7bceed80cbddf3b42e3c2401a91c1fb1991485
Alan Denniston <alan.denniston@gmail.com>
2026-05-24 07:34:13 +0000

    tune(heal_mech2): speed up baseline rotation cadence

--- games/Guild Wars 2/specs/heal_mech2.py
+++ games/Guild Wars 2/specs/heal_mech2.py
@@ -17,7 +17,7 @@ def healing_mechanist_rotation(stop_event):
         button_mash('1', stop_check=lambda: check_stop_condition(stop_event))
         button_mash('2', stop_check=lambda: check_stop_condition(stop_event))
         button_mash('3', stop_check=lambda: check_stop_condition(stop_event))
-        time.sleep(0.5)
+        time.sleep(0.30)
         
         if check_stop_condition(stop_event):
             break
@@ -35,60 +35,60 @@ def healing_mechanist_rotation(stop_event):
 
             if pixel_get_color(2799, 1015) != (0, 0, 0):
                 if not button_mash(key_mapping['numpad5'], stop_check=lambda: check_stop_condition(stop_event)): break  # Super Elixir
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
                 continue
 
             if not button_mash(key_mapping['numpad0'], stop_check=lambda: check_stop_condition(stop_event)): break  # Switch to Mortar
-            time.sleep(0.5)
+            time.sleep(0.30)
             if check_stop_condition(stop_event): break
 
         elif pixel_get_color(3080, 1035) == (255, 255, 255):
             if pixel_get_color(2799, 1015) != (0, 0, 0):
                 if not button_mash(key_mapping['numpad5'], stop_check=lambda: check_stop_condition(stop_event)): break  # Elixir Shell
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
             else:
                 if not button_mash(key_mapping['numpad6'], stop_check=lambda: check_stop_condition(stop_event)): break  # Switch to MedKit
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
-            time.sleep(0.5)
+            time.sleep(0.30)
             if check_stop_condition(stop_event): break
 
         elif pixel_get_color(2960, 1035) == (255, 255, 255):
             if pixel_get_color(2799, 1015) != (0, 0, 0):
                 if not button_mash(key_mapping['numpad5'], stop_check=lambda: check_stop_condition(stop_event)): break  # Infusion Bomb
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
             else:
                 if not button_mash(key_mapping['f1'], stop_check=lambda: check_stop_condition(stop_event)): break  # Weapon Swap
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
-            time.sleep(0.5)
+            time.sleep(0.30)
             if check_stop_condition(stop_event): break
 
         else:
             if pixel_get_color(2742, 1015) != (0, 0, 0):
                 if not button_mash(key_mapping['numpad4'], stop_check=lambda: check_stop_condition(stop_event)): break  # Magnetic Shield
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
             elif pixel_get_color(2799, 1015) != (0, 0, 0):
                 if not button_mash(key_mapping['numpad5'], stop_check=lambda: check_stop_condition(stop_event)): break  # Static Shield
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
                 
                 if not button_mash(key_mapping['numpad2'], stop_check=lambda: check_stop_condition(stop_event)): break  # Energizing Slam
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
             else:
                 if not button_mash(key_mapping['numpad7'], stop_check=lambda: check_stop_condition(stop_event)): break  # Switch to Elixir Gun
-                time.sleep(0.5)
+                time.sleep(0.30)
                 if check_stop_condition(stop_event): break
-            time.sleep(0.5)
+            time.sleep(0.30)
             if check_stop_condition(stop_event): break
 
         if not button_mash(key_mapping['numpad8'], stop_check=lambda: check_stop_condition(stop_event)): break
-        time.sleep(0.5)
+        time.sleep(0.30)
         if check_stop_condition(stop_event): break
 
 def run(stop_event):
